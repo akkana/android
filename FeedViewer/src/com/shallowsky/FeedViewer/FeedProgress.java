ce525fade75eadbdc3099642598f46138bee77cd
Akkana Peck <akkana@shallowsky.com>
2015-09-12 19:16:02 -0600

    Comment out the two scrollTos in FeedProgress that were making the screen flash and delay so badly

--- FeedViewer/src/com/shallowsky/FeedViewer/FeedProgress.java
+++ FeedViewer/src/com/shallowsky/FeedViewer/FeedProgress.java
@@ -18,25 +18,37 @@ public class FeedProgress {
     public void log(String s) {
         mTextView.append(s);
 
-        /*
+        // The rest of this class is an attempt to get the dialog
+        // to scroll to the bottom any time it's shown,
+        // preferably without flashing horribly or hanging the UI,
+        // which most of these solutions do if they do anything at all.
+
         // Scroll to the bottom. http://stackoverflow.com/a/4612082
         // (The easier and top-rated solution in that thread didn't work
         // because layout was null.)
-           Mostly doesn't work, though.
-        */
         mScrollView.post(new Runnable() {
             public void run() {
                 mScrollView.fullScroll(View.FOCUS_DOWN);
             }
         });
+
         /* Here are some more non-working solutions, from
          * http://stackoverflow.com/questions/19826693/how-can-i-make-a-textview-automatically-scroll-as-i-add-more-lines-of-text
          * The first one doesn't even compile because ScrollView has no
          * method setSelection().
          */
         //mTextView.setSelection(mTextView.getText().length());
+
+        /* These two sorta work, but make the screen flash so much
+         * you can't read it and make for a huge delay between when
+         * you show the dialog and when you can do anything else.
+         * XXX Maybe each of these scrollTo events takes a finite amount
+         * of time and the events aren't collapsed together?
+         * Maybe if we could batch them by timestamp somehow?
         mTextView.scrollTo(0, Integer.MAX_VALUE);
         mScrollView.scrollTo(0, Integer.MAX_VALUE);
+         */
+
         /* Next attempt crashes with a NullPointerException on the first line:
         final int scrollAmount = mTextView.getLayout().getLineTop(mTextView.getLineCount()) - mTextView.getHeight();
         // if there is no need to scroll, scrollAmount will be <=0
