f18fc03e636e3eaa3287df32b11c256aa666be14
Akkana Peck <akkana@shallowsky.com>
2017-02-11 15:21:17 -0700

    Use a LocationListener as well as LastKnown

--- BBAgrid/src/com/shallowsky/BBAgrid/BBAgrid.java
+++ BBAgrid/src/com/shallowsky/BBAgrid/BBAgrid.java
@@ -19,19 +19,20 @@ import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 
-//import com.google.android.gms.location.LocationResult;
-
 import java.util.Set;
 import java.util.HashSet;
 
 public class BBAgrid extends Activity {
 
     private static final int REQUEST_ENABLE_BT = 1;
+    private static final long MIN_TIME_BETWEEN_UPDATES = 30000;   // msec
+    private static final float MIN_DISTANCE_CHANGE = 50;          // m
 
     Button mBtnCheckLoc;
     TextView mOutput;
-    //MyLocation mGetLoc;
-    //LocationResult mLocationResult = null;
+    LocationManager mLocMgr;
+
+    int mSequence = 0;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -41,53 +42,28 @@ public class BBAgrid extends Activity {
         mBtnCheckLoc = (Button)findViewById(R.id.checkloc);
         mOutput = (TextView)findViewById(R.id.output);
 
-        /*
-        mGetLoc = new MyLocation();
+        mLocMgr = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
+        mLocMgr.requestLocationUpdates(LocationManager.GPS_PROVIDER,
+                                       MIN_TIME_BETWEEN_UPDATES,
+                                       MIN_DISTANCE_CHANGE,
+                                       new MyLocationListener());
 
-        mLocationResult = new LocationResult() {
-            @Override
-            public void gotLocation(Location loc) {
-                mOutput.setText(loc.getLatitude() + "\n"
-                                + loc.getLongitude() + "\n"
-                                + loc.getAltitude());
-            }
-        };
-        */
-
-        updateLoc();
+        //updateLoc();
 
         mBtnCheckLoc.setOnClickListener(btnUpdateOnClickListener);
-
-        /*
-        registerReceiver(ActionFoundReceiver,
-          new IntentFilter(BluetoothDevice.ACTION_FOUND));
-         */
     }
 
-    /*
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        unregisterReceiver(ActionFoundReceiver);
+    public void updateText(Location loc, String where) {
+        // This causes crashes:
+        //mOutput.setText(String.format("(%d %s)\n\n%.4s\n%.4f\n%.0f",
+        // but positional formatting doesn't:
+        mOutput.setText(String.format("(%1$s %2$s)\n\n%3$s\n%4$f\n%5$f",
+                                      where, mSequence,
+                                      loc.getLongitude(),
+                                      loc.getLatitude(),
+                                      loc.getAltitude()));
+        mSequence += 1;
     }
-    */
-
-    private void updateLoc() {
-        LocationManager lm = (LocationManager)getSystemService(Context.LOCATION_SERVICE);
-        Location loc = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-        mOutput.setText("\n\n\n"
-                        + loc.getLatitude() + "\n"
-                        + loc.getLongitude() + "\n"
-                        + loc.getAltitude());
-    }
-
-    /*
-    private void updateLoc() {
-        LocationResult loc;
-        if (!mGetLoc.getLocation(this, loc))
-            mOutput.setText("Couldn't get location");
-    }
-    */
 
     private Button.OnClickListener btnUpdateOnClickListener
         = new Button.OnClickListener() {
@@ -96,42 +72,15 @@ public class BBAgrid extends Activity {
                     updateLoc();
                 }};
 
-    /*
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode,
-                                    Intent data) {
-        if (requestCode == REQUEST_ENABLE_BT) {
-            CheckBlueToothState();
-        }
+    private void updateLoc() {
+        Location loc = mLocMgr.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        updateText(loc, "button");
     }
-
-    private final BroadcastReceiver ActionFoundReceiver
-        = new BroadcastReceiver() {
-            @Override
-            public void onReceive(Context context, Intent intent) {
-                String action = intent.getAction();
-                if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-                    BluetoothDevice device
-                      = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-                    if (mMacSet.contains(device.getAddress()))
-                        return;
-                    mMacSet.add(device.getName());
-                    mBTArrayAdapter.add(device.getName() + "\n"
-                                        + device.getAddress());
-                    mBTArrayAdapter.notifyDataSetChanged();
-                }
-            }};
-    */
 
     private class MyLocationListener implements LocationListener {
 
-        public void onLocationChanged(Location location) {
-            String message = String.format(
-                                           "New Location \n Longitude: %1$s \n Latitude: %2$s",
-                                           location.getLongitude(), location.getLatitude()
-                                           );
-
-            Toast.makeText(BBAgrid.this, message, Toast.LENGTH_LONG).show();
+        public void onLocationChanged(Location loc) {
+            updateText(loc, "loc changed");
         }
 
         public void onStatusChanged(String s, int i, Bundle b) {
