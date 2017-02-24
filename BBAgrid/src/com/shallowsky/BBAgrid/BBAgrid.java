9bff2ef554cb924f34cfc5936eef0cee8e487fd8
Akkana Peck <akkana@shallowsky.com>
2017-02-23 19:16:15 -0700

    Calculate and display the distance to neighboring grid blocks. Update immediately upon startup. Reduce update frequency to every 15 seconds.

--- BBAgrid/src/com/shallowsky/BBAgrid/BBAgrid.java
+++ BBAgrid/src/com/shallowsky/BBAgrid/BBAgrid.java
@@ -1,3 +1,20 @@
+/* XXX TO DO:
+   - Show distance to nearest other grid blocks.
+   - Change the MIN_DISTANCE_CHANGE if we're near a block boundary:
+     maybe to something like half the distance to the nearest boundary,
+     or 10m if we're within 100m of a boundary.
+     (Mouser says his GPS noise is between 5 and 9 meters w/good reception.)
+   - investigate going to sleep when the app isn't in the foreground,
+     and updating position upon being displayed.
+     (But maybe don't make that mandatory, see next item.)
+   - Notification in the status bar showing current grid.
+     https://developer.android.com/guide/topics/ui/notifiers/notifications.html
+   - Draw a picture of where we are in the grid block relative to other
+     squares (Canvas?
+     https://developer.android.com/reference/android/graphics/drawable/package-summary.html
+     https://developer.android.com/training/custom-views/custom-drawing.html
+ */
+
 package com.shallowsky.BBAgrid;
 
 import android.os.Bundle;
@@ -31,8 +48,10 @@ import java.util.ArrayList;
 public class BBAgrid extends Activity {
 
     private static final int REQUEST_ENABLE_BT = 1;
-    private static final long MIN_TIME_BETWEEN_UPDATES = 30000;   // msec
-    private static final float MIN_DISTANCE_CHANGE = 50;          // m
+    private static final long MIN_TIME_BETWEEN_UPDATES = 15000;   // msec
+    private static final float MIN_DISTANCE_CHANGE = 10;          // m
+
+    public static final double EARTH_RADIUS = 6371000.;           // m
 
     Button mBtnCheckLoc;
     TextView mOutput;
@@ -47,16 +66,21 @@ public class BBAgrid extends Activity {
 
     int mSequence = 0;    // Mostly for debugging
 
+    // Current grid block and distance to adjacent blocks:
+    int mCurRow;
+    int mCurCol;
+    double mWestDist;
+    double mEastDist;
+    double mNorthDist;
+    double mSouthDist;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        // All the grid blocks and their coordinates.
-        //Dictionary mGridBlocks = new Hashtable();
-        //mGridBlocks.put("11", "Java");
-
         setContentView(R.layout.main);
 
+        // All the grid blocks and their coordinates.
         mNWCoords = new double[][][] {
             {    // row 0
                 { 0., 0. },
@@ -229,42 +253,111 @@ public class BBAgrid extends Activity {
                                        MIN_DISTANCE_CHANGE,
                                        new MyLocationListener());
 
-        //updateLoc();
+        updateLoc();
 
         mBtnCheckLoc.setOnClickListener(btnUpdateOnClickListener);
     }
 
-    public String locationToGridBlock(Location loc) {
+    public void locationToGridBlock(Location loc) {
         // Given a location, figure out what LA-BBA block it's in
-        // and return that as a string.
+        // and return that as a string. Also fills in mCurRow, mCurCol.
         double lat = loc.getLatitude();
         double lon = loc.getLongitude();
 
         for (int row = 1; row < mNumRows+1; ++row) {
             for (int col = mStartColumn[row-1];
                  col < mStartColumn[row-1] + mColsInRow[row-1]; ++col) {
-                double west = (mNWCoords[row][col][0] + mNWCoords[row+1][col][0]) / 2.;
-                double east = (mNWCoords[row][col+1][0] + mNWCoords[row+1][col+1][0]) / 2.;
-                double north = (mNWCoords[row][col][1] + mNWCoords[row][col+1][1]) / 2.;
-                double south = (mNWCoords[row+1][col][1] + mNWCoords[row+1][col+1][1]) / 2.;
-                if (lat <= north && lat > south && lon >= west && lon < east)
-                    return String.format("%1$d%2$d", row, col);
+                double west = (mNWCoords[row][col][0]
+                               + mNWCoords[row+1][col][0]) / 2.;
+                double east = (mNWCoords[row][col+1][0]
+                               + mNWCoords[row+1][col+1][0]) / 2.;
+                double north = (mNWCoords[row][col][1]
+                                + mNWCoords[row][col+1][1]) / 2.;
+                double south = (mNWCoords[row+1][col][1]
+                                + mNWCoords[row+1][col+1][1]) / 2.;
+                if (lat <= north && lat > south && lon >= west && lon < east) {
+                    mCurRow = row;
+                    mCurCol = col;
+                    findBlockDistances(lat, lon);
+                    return;
+                }
             }
         }
-        return "Outside grid";
+        mCurRow = mCurCol = -1;
+    }
+
+    public void findBlockDistances(double lat, double lon) {
+        // After determining mCurRow and mCurCol, calculate
+        // mWestDist, mEastDist, mNorthDist, mSouthDist.
+        double westGridLon = (mNWCoords[mCurRow][mCurCol][0]
+                              + mNWCoords[mCurRow+1][mCurCol][0]) / 2.;
+        mWestDist = haversine_dist(lat, westGridLon, lat, lon);
+
+        double eastGridLon = (mNWCoords[mCurRow][mCurCol+1][0]
+                              + mNWCoords[mCurRow+1][mCurCol+1][0]) / 2.;
+        mEastDist = haversine_dist(lat, eastGridLon, lat, lon);
+
+        double northGridLat = (mNWCoords[mCurRow][mCurCol][1]
+                               + mNWCoords[mCurRow][mCurCol+1][1]) / 2.;
+        mNorthDist = haversine_dist(northGridLat, lon, lat, lon);
+
+        double southGridLat = (mNWCoords[mCurRow+1][mCurCol][1]
+                               + mNWCoords[mCurRow+1][mCurCol+1][1]) / 2.;
+        mSouthDist = haversine_dist(southGridLat, lon, lat, lon);
+    }
+
+    public double haversine_dist(double lat1, double lon1,
+                                 double lat2, double lon2) {
+        // Haversine distance between two points, expressed in meters.
+
+        double d_lat = Math.toRadians(lat1 - lat2);
+        double d_lon = Math.toRadians(lon1 - lon2);
+        lat1 = Math.toRadians(lat1);
+        lat2 = Math.toRadians(lat2);
+
+        double a = Math.sin(d_lat / 2.) * Math.sin(d_lat / 2.) +
+            Math.sin(d_lon / 2.) * Math.sin(d_lon / 2.) *
+            Math.cos(lat1) * Math.cos(lat2);
+        double c = 2. * Math.atan2(Math.sqrt(a), Math.sqrt(1. - a));
+        return EARTH_RADIUS * c;
     }
 
     public void updateText(Location loc, String where) {
-        String block = locationToGridBlock(loc);
+        locationToGridBlock(loc);
+        // mCurRow and mCurCol, plus mWestDist, etc., are now set.
+
+        if (mCurRow < 0 || mCurCol < 0) {
+            mOutput.setText("Outside the grid");
+        }
 
         // This causes crashes:
-        //mOutput.setText(String.format("(%d %s)\n\n%.4s\n%.4f\n%.0f",
-        // but positional formatting doesn't:
+        //    String.format("(%d %s)\n\n%.4s\n%.4f\n%.0f",
+        // but positional formatting doesn't.
+
+        /*
         mOutput.setText(String.format("Block %1$s\n\n%2$f\n%3$f\n%4$d m\n\n(%5$s %6$d)",
                                       block,
                                       loc.getLongitude(), loc.getLatitude(),
                                       (int)(loc.getAltitude()),
                                       where, mSequence));
+         */
+        String output = String.format("Block %1$d%2$d\n", mCurRow, mCurCol);
+        if (mCurRow >= 0 && mCurCol >= 0) {
+            if (mWestDist < mEastDist)
+                output += String.format("\n%1$dm W to %2$d%3$d",
+                                        (int)mWestDist, mCurRow, mCurCol-1);
+            else
+                output += String.format("\n%1$dm E to %2$d%3$d",
+                                        (int)mEastDist, mCurRow, mCurCol+1);
+            if (mNorthDist < mSouthDist)
+                output += String.format("\n%1$dm N to %2$d%3$d",
+                                        (int)mNorthDist, mCurRow-1, mCurCol);
+            else
+                output += String.format("\n%1$dm S to %2$d%3$d",
+                                        (int)mSouthDist, mCurRow+1, mCurCol);
+        }
+        mOutput.setText(output);
+
         mSequence += 1;
     }
 
@@ -287,6 +380,7 @@ public class BBAgrid extends Activity {
         }
 
         public void onStatusChanged(String providerStr, int status, Bundle b) {
+            /*
             String s;
             if (status == LocationProvider.OUT_OF_SERVICE)
                 s = "Status changed: Out of service";
@@ -297,18 +391,23 @@ public class BBAgrid extends Activity {
             else
                 s = "Provider status changed to " + status;
             Toast.makeText(BBAgrid.this, s, Toast.LENGTH_LONG).show();
-        }
-
-        public void onProviderDisabled(String s) {
-            Toast.makeText(BBAgrid.this,
-                           "Provider disabled by the user. GPS turned off",
-                           Toast.LENGTH_LONG).show();
+             */
         }
 
         public void onProviderEnabled(String s) {
+            /*
             Toast.makeText(BBAgrid.this,
                            "Provider enabled by the user. GPS turned on",
                            Toast.LENGTH_LONG).show();
+             */
+        }
+
+        public void onProviderDisabled(String s) {
+            /*
+            Toast.makeText(BBAgrid.this,
+                           "Provider disabled by the user. GPS turned off",
+                           Toast.LENGTH_LONG).show();
+             */
         }
     }
 }
